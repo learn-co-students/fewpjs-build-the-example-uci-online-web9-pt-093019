7e3c99fec39ed015c82a8e85153cfc6026d80459
Alice Freda <alicefreda@gmail.com>
2020-05-09 11:34:43 -0700

    Done.

--- main.js
+++ main.js
@@ -2,15 +2,38 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const modal = document.getElementById('modal')
+modal.className = 'hidden'
+const heartBtns = document.getElementsByClassName('like-glyph')
 
+document.addEventListener('DOMContentLoaded', () => {
+  for(const button of heartBtns) {
+    button.addEventListener('click',(e) => {
+    let heart = e.target
+    //mimicservercall function
+    mimicServerCall()
+    .then(() => {
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART
+        heart.className = "activated-heart"
+      } else {
+        heart.innerText = EMPTY_HEART
+        heart.className = ''
+      }
 
-
-
+    })
+    .catch(() => {
+      modal.className = ''
+      setTimeout(() => {
+        modal.className = 'hidden'
+      }, 5000)
+    })
+  })
+  }
+})
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
-
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
