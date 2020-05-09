756743653c5c20b8548b52968985608480fd62a2
ACAPP-dev <acapp909@gmail.com>
2020-05-09 13:42:54 -0700

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,41 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorDiv = document.getElementById('modal')
+const errorTextArea = document.getElementById('modal-message')
+const comments = document.getElementsByClassName('media-post')
 
+for (const commentElement of comments) {
+  const likeElement = commentElement.getElementsByClassName('like-glyph')[0]
+  likeElement.addEventListener('click', likeEvent)
+}
 
+function likeEvent(event) {
+  const heartElement = event.target
+  mimicServerCall()
+  .then (resp => like(heartElement))
+  .catch ((error) => {
+    errorDiv.classList.remove('hidden')
+    errorTextArea.innerText = error
+    setTimeout(timeOut, 5000)
+  })
+}
+
+function like(heartElement) {
+  
+  if (heartElement.className == 'like-glyph activated-heart') {
+    heartElement.innerText = EMPTY_HEART
+    heartElement.className = 'like-glyph'
+  }
+  else {
+    heartElement.innerText = FULL_HEART
+    heartElement.className = 'like-glyph activated-heart'
+  }
+}
+
+function timeOut() {
+  errorDiv.classList.add('hidden')
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
